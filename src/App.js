a518e19a00f897ddd448e7f2834ef2e533645f04
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-09-25 01:55:29 +0300

    completed react hooks use effect assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
+  function handleCount() {
+    setCount(count + 1)
+  }
+
+  function handleText(event) {
+    setText(event.target.value)
+  }
+
   useEffect(() => {
+    document.title = text
     console.log("useEffect called");
-  });
+  }, [count, text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000)
+  }, [])
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={handleCount}>Click Me {count}</button>
+      <input type="text" placeholder="Type away..." value={text} onChange={handleText}/>
+      <DogPics />
+    </div>
+  )
 }
 
 export default App;
